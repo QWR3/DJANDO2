289ceca2650711db3199a036e08e724685fdfbf6
QWR3 <anton004@protonmail.com>
2021-12-30 07:34:09 +0200

    lesson 2.1

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+from cars.models import CarModel
+
+
+class CarListCreateView(APIView):
+    def get(self, *args, **kwargs):
+        cars = CarModel.objects.all().values()
+        return Response(cars)
+
+    def post(self, *args, **kwargs):
+        data = self.request.data.dict()
+        car = CarModel.objects.create(**data)
+        return Response(model_to_dict(car))
+
+
+class CarReadUpdateDeleteView(APIView):
+    def get(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        exists = CarModel.objects.filter(pk=pk).exists()
+        if not exists:
+            return Response("Go go away")
+        car = CarModel.objects.get(pk=pk)
+        return Response(model_to_dict(car))
+
+    def put(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        exists = CarModel.objects.filter(pk=pk).exists()
+        if not exists:
+            return Response("yps")
+        data = self.request.data.dict()
+        CarModel.objects.filter(pk=pk).update(**data)
+        car = CarModel.objects.get(pk=pk)
+        print(car)
+        return Response(model_to_dict(car))
+
+    def delete(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        exists = CarModel.objects.filter(pk=pk).exists()
+        if not exists:
+            return Response('go go away')
+        CarModel.objects.filter(pk=pk).delete()
+        return Response('')
