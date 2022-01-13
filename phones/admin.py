e6563455282fc9898ab84967e850e376b80f289f
QWR3 <anton004@protonmail.com>
2022-01-13 15:26:12 +0200

    lesson 3.1

--- phones/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from phones.models import PhoneModel
-
-
-@admin.register(PhoneModel)
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ['id', 'brand', 'model', 'camera', 'RAM', 'CPU', 'diagonal', 'price']
-
